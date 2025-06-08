2642895566d4f4f208ab7c209dd727c56429b346
Shubham <shubhammanohare88@gmail.com>
2025-06-09 00:48:13 +0530

    sdfdsf

--- Nexus_Photography_API/Program.cs
+++ Nexus_Photography_API/Program.cs
@@ -5,12 +5,11 @@ using Nexus_Photography_API.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configure DbContext with SQL Server and retry policy
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(
@@ -25,12 +24,12 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
         });
 });
 
-// Dependency Injection for your Repository
-builder.Services.AddTransient<IRepository, Repository>();
+// Register custom services (Dependency Injection)
+builder.Services.AddScoped<IRepository, Repository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
